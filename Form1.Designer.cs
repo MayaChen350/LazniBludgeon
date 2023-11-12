35eb0547ab08dff4dac062d8de259612db91b7e4
Maya <145165822+MayaChen350@users.noreply.github.com>
2023-11-12 02:14:29 -0500

    Next step is the PoC

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,17 @@
             this.p2SecondaryCard2 = new System.Windows.Forms.PictureBox();
             this.p2SecondaryCard1 = new System.Windows.Forms.PictureBox();
             this.p2PlayerCard = new System.Windows.Forms.PictureBox();
+            this.p1SecondaryCard4 = new System.Windows.Forms.PictureBox();
+            this.p1SecondaryCard5 = new System.Windows.Forms.PictureBox();
+            this.p1SecondaryCard6 = new System.Windows.Forms.PictureBox();
+            this.p1SecondaryCard7 = new System.Windows.Forms.PictureBox();
+            this.p2SecondaryCard4 = new System.Windows.Forms.PictureBox();
+            this.p2SecondaryCard5 = new System.Windows.Forms.PictureBox();
+            this.p2SecondaryCard6 = new System.Windows.Forms.PictureBox();
+            this.p2SecondaryCard7 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startGameToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.p1PlayerCard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard2)).BeginInit();
@@ -44,13 +55,22 @@
             ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.p2PlayerCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard7)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // p1PlayerCard
             // 
             this.p1PlayerCard.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p1PlayerCard.BackColor = System.Drawing.Color.Cyan;
-            this.p1PlayerCard.Location = new System.Drawing.Point(91, 287);
+            this.p1PlayerCard.Location = new System.Drawing.Point(115, 409);
             this.p1PlayerCard.Name = "p1PlayerCard";
             this.p1PlayerCard.Size = new System.Drawing.Size(80, 109);
             this.p1PlayerCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -61,7 +81,7 @@
             // 
             this.p1SecondaryCard1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p1SecondaryCard1.BackColor = System.Drawing.Color.Red;
-            this.p1SecondaryCard1.Location = new System.Drawing.Point(236, 287);
+            this.p1SecondaryCard1.Location = new System.Drawing.Point(236, 409);
             this.p1SecondaryCard1.Name = "p1SecondaryCard1";
             this.p1SecondaryCard1.Size = new System.Drawing.Size(80, 109);
             this.p1SecondaryCard1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -72,7 +92,7 @@
             // 
             this.p1SecondaryCard2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p1SecondaryCard2.BackColor = System.Drawing.Color.Red;
-            this.p1SecondaryCard2.Location = new System.Drawing.Point(374, 287);
+            this.p1SecondaryCard2.Location = new System.Drawing.Point(374, 409);
             this.p1SecondaryCard2.Name = "p1SecondaryCard2";
             this.p1SecondaryCard2.Size = new System.Drawing.Size(80, 109);
             this.p1SecondaryCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -83,7 +103,7 @@
             // 
             this.p1SecondaryCard3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p1SecondaryCard3.BackColor = System.Drawing.Color.Red;
-            this.p1SecondaryCard3.Location = new System.Drawing.Point(508, 287);
+            this.p1SecondaryCard3.Location = new System.Drawing.Point(508, 409);
             this.p1SecondaryCard3.Name = "p1SecondaryCard3";
             this.p1SecondaryCard3.Size = new System.Drawing.Size(80, 109);
             this.p1SecondaryCard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -94,7 +114,7 @@
             // 
             this.p2SecondaryCard3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p2SecondaryCard3.BackColor = System.Drawing.Color.Yellow;
-            this.p2SecondaryCard3.Location = new System.Drawing.Point(236, 31);
+            this.p2SecondaryCard3.Location = new System.Drawing.Point(236, 153);
             this.p2SecondaryCard3.Name = "p2SecondaryCard3";
             this.p2SecondaryCard3.Size = new System.Drawing.Size(80, 109);
             this.p2SecondaryCard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -105,7 +125,7 @@
             // 
             this.p2SecondaryCard2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p2SecondaryCard2.BackColor = System.Drawing.Color.Yellow;
-            this.p2SecondaryCard2.Location = new System.Drawing.Point(374, 31);
+            this.p2SecondaryCard2.Location = new System.Drawing.Point(374, 153);
             this.p2SecondaryCard2.Name = "p2SecondaryCard2";
             this.p2SecondaryCard2.Size = new System.Drawing.Size(80, 109);
             this.p2SecondaryCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -116,7 +136,7 @@
             // 
             this.p2SecondaryCard1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p2SecondaryCard1.BackColor = System.Drawing.Color.Yellow;
-            this.p2SecondaryCard1.Location = new System.Drawing.Point(508, 31);
+            this.p2SecondaryCard1.Location = new System.Drawing.Point(508, 153);
             this.p2SecondaryCard1.Name = "p2SecondaryCard1";
             this.p2SecondaryCard1.Size = new System.Drawing.Size(80, 109);
             this.p2SecondaryCard1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -127,18 +147,146 @@
             // 
             this.p2PlayerCard.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.p2PlayerCard.BackColor = System.Drawing.Color.Lime;
-            this.p2PlayerCard.Location = new System.Drawing.Point(646, 31);
+            this.p2PlayerCard.Location = new System.Drawing.Point(646, 153);
             this.p2PlayerCard.Name = "p2PlayerCard";
             this.p2PlayerCard.Size = new System.Drawing.Size(80, 109);
             this.p2PlayerCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.p2PlayerCard.TabIndex = 8;
             this.p2PlayerCard.TabStop = false;
             // 
+            // p1SecondaryCard4
+            // 
+            this.p1SecondaryCard4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p1SecondaryCard4.BackColor = System.Drawing.Color.Maroon;
+            this.p1SecondaryCard4.Location = new System.Drawing.Point(236, 573);
+            this.p1SecondaryCard4.Name = "p1SecondaryCard4";
+            this.p1SecondaryCard4.Size = new System.Drawing.Size(80, 109);
+            this.p1SecondaryCard4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1SecondaryCard4.TabIndex = 9;
+            this.p1SecondaryCard4.TabStop = false;
+            this.p1SecondaryCard4.Visible = false;
+            // 
+            // p1SecondaryCard5
+            // 
+            this.p1SecondaryCard5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p1SecondaryCard5.BackColor = System.Drawing.Color.Maroon;
+            this.p1SecondaryCard5.Location = new System.Drawing.Point(374, 573);
+            this.p1SecondaryCard5.Name = "p1SecondaryCard5";
+            this.p1SecondaryCard5.Size = new System.Drawing.Size(80, 109);
+            this.p1SecondaryCard5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1SecondaryCard5.TabIndex = 10;
+            this.p1SecondaryCard5.TabStop = false;
+            this.p1SecondaryCard5.Visible = false;
+            // 
+            // p1SecondaryCard6
+            // 
+            this.p1SecondaryCard6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p1SecondaryCard6.BackColor = System.Drawing.Color.Maroon;
+            this.p1SecondaryCard6.Location = new System.Drawing.Point(508, 573);
+            this.p1SecondaryCard6.Name = "p1SecondaryCard6";
+            this.p1SecondaryCard6.Size = new System.Drawing.Size(80, 109);
+            this.p1SecondaryCard6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1SecondaryCard6.TabIndex = 11;
+            this.p1SecondaryCard6.TabStop = false;
+            this.p1SecondaryCard6.Visible = false;
+            // 
+            // p1SecondaryCard7
+            // 
+            this.p1SecondaryCard7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p1SecondaryCard7.BackColor = System.Drawing.Color.Maroon;
+            this.p1SecondaryCard7.Location = new System.Drawing.Point(646, 573);
+            this.p1SecondaryCard7.Name = "p1SecondaryCard7";
+            this.p1SecondaryCard7.Size = new System.Drawing.Size(80, 109);
+            this.p1SecondaryCard7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1SecondaryCard7.TabIndex = 12;
+            this.p1SecondaryCard7.TabStop = false;
+            this.p1SecondaryCard7.Visible = false;
+            // 
+            // p2SecondaryCard4
+            // 
+            this.p2SecondaryCard4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p2SecondaryCard4.BackColor = System.Drawing.Color.Olive;
+            this.p2SecondaryCard4.Location = new System.Drawing.Point(508, 12);
+            this.p2SecondaryCard4.Name = "p2SecondaryCard4";
+            this.p2SecondaryCard4.Size = new System.Drawing.Size(80, 109);
+            this.p2SecondaryCard4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2SecondaryCard4.TabIndex = 13;
+            this.p2SecondaryCard4.TabStop = false;
+            this.p2SecondaryCard4.Visible = false;
+            // 
+            // p2SecondaryCard5
+            // 
+            this.p2SecondaryCard5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p2SecondaryCard5.BackColor = System.Drawing.Color.Olive;
+            this.p2SecondaryCard5.Location = new System.Drawing.Point(374, 12);
+            this.p2SecondaryCard5.Name = "p2SecondaryCard5";
+            this.p2SecondaryCard5.Size = new System.Drawing.Size(80, 109);
+            this.p2SecondaryCard5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2SecondaryCard5.TabIndex = 14;
+            this.p2SecondaryCard5.TabStop = false;
+            this.p2SecondaryCard5.Visible = false;
+            // 
+            // p2SecondaryCard6
+            // 
+            this.p2SecondaryCard6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p2SecondaryCard6.BackColor = System.Drawing.Color.Olive;
+            this.p2SecondaryCard6.Location = new System.Drawing.Point(236, 12);
+            this.p2SecondaryCard6.Name = "p2SecondaryCard6";
+            this.p2SecondaryCard6.Size = new System.Drawing.Size(80, 109);
+            this.p2SecondaryCard6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2SecondaryCard6.TabIndex = 15;
+            this.p2SecondaryCard6.TabStop = false;
+            this.p2SecondaryCard6.Visible = false;
+            // 
+            // p2SecondaryCard7
+            // 
+            this.p2SecondaryCard7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.p2SecondaryCard7.BackColor = System.Drawing.Color.Olive;
+            this.p2SecondaryCard7.Location = new System.Drawing.Point(115, 12);
+            this.p2SecondaryCard7.Name = "p2SecondaryCard7";
+            this.p2SecondaryCard7.Size = new System.Drawing.Size(80, 109);
+            this.p2SecondaryCard7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2SecondaryCard7.TabIndex = 16;
+            this.p2SecondaryCard7.TabStop = false;
+            this.p2SecondaryCard7.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startGameToolStripMenuItem
+            // 
+            this.startGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startGameToolStripMenuItem1});
+            this.startGameToolStripMenuItem.Name = "startGameToolStripMenuItem";
+            this.startGameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.startGameToolStripMenuItem.Text = "Menu";
+            // 
+            // startGameToolStripMenuItem1
+            // 
+            this.startGameToolStripMenuItem1.Name = "startGameToolStripMenuItem1";
+            this.startGameToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.startGameToolStripMenuItem1.Text = "&New game";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 694);
+            this.Controls.Add(this.p2SecondaryCard4);
+            this.Controls.Add(this.p2SecondaryCard7);
+            this.Controls.Add(this.p2SecondaryCard6);
+            this.Controls.Add(this.p2SecondaryCard5);
+            this.Controls.Add(this.p1SecondaryCard7);
+            this.Controls.Add(this.p1SecondaryCard6);
+            this.Controls.Add(this.p1SecondaryCard5);
+            this.Controls.Add(this.p1SecondaryCard4);
             this.Controls.Add(this.p2PlayerCard);
             this.Controls.Add(this.p2SecondaryCard1);
             this.Controls.Add(this.p2SecondaryCard2);
@@ -147,6 +295,8 @@
             this.Controls.Add(this.p1SecondaryCard2);
             this.Controls.Add(this.p1SecondaryCard1);
             this.Controls.Add(this.p1PlayerCard);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "The Lazni Card Game (BETA)";
             ((System.ComponentModel.ISupportInitialize)(this.p1PlayerCard)).EndInit();
@@ -157,7 +307,18 @@
             ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.p2PlayerCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1SecondaryCard7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2SecondaryCard7)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -170,6 +331,17 @@
         private System.Windows.Forms.PictureBox p2SecondaryCard2;
         private System.Windows.Forms.PictureBox p2SecondaryCard1;
         private System.Windows.Forms.PictureBox p2PlayerCard;
+        private System.Windows.Forms.PictureBox p1SecondaryCard4;
+        private System.Windows.Forms.PictureBox p1SecondaryCard5;
+        private System.Windows.Forms.PictureBox p1SecondaryCard6;
+        private System.Windows.Forms.PictureBox p1SecondaryCard7;
+        private System.Windows.Forms.PictureBox p2SecondaryCard4;
+        private System.Windows.Forms.PictureBox p2SecondaryCard5;
+        private System.Windows.Forms.PictureBox p2SecondaryCard6;
+        private System.Windows.Forms.PictureBox p2SecondaryCard7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startGameToolStripMenuItem1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -38,14 +38,14 @@ namespace LazniCardGame
             }
         }
 
-        class SpecialdCard
+        class SpecialCard
         {
             public int hp;
             public int atk;
             //ability1
             //ability2
 
-            public SpecialdCard(int Hp, int Atk)
+            public SpecialCard(int Hp, int Atk)
             {
                 hp = Hp;
                 atk = Atk;
@@ -61,25 +61,132 @@ namespace LazniCardGame
         #endregion
 
         String[] PlayerCards = { "Allemanie", "Allemapon", "Almahad", "Anglestan", "Canalgeria", "Fitalie", "Garulmonie", "Khenaga", "Mulretonie", "Nitralvie", "Qaland", "Slovannoya", "Starvas", "TheLeaf", "Traicere", "Yedesna" };
+
+        #region CARDS (110 CARDS)
+        // Player cards (16 cards)
         
-        #region CARDS
-        // Player cards
-        PlayerCard Allemanie = new PlayerCard(3500, 250);
+        //RESERVED FOR THE PROOF OF CONCEPT
+        PlayerCard Slovannoya = new PlayerCard(2850, 350);
         PlayerCard Allemapon = new PlayerCard(3000, 0);
-        PlayerCard Almahad = new PlayerCard(3500, 250);
         PlayerCard Anglestan = new PlayerCard(3450, 100);
+        PlayerCard Garulmonie = new PlayerCard(3400, 150);
+
+        /*PlayerCard Allemanie = new PlayerCard(3500, 250);
+        //Allemapon
+        PlayerCard Almahad = new PlayerCard(3500, 250);
+        //Anglestan
         PlayerCard Canalgeria = new PlayerCard(3500, 200);
         PlayerCard Fitalie = new PlayerCard(4000, 250);
-        PlayerCard Garulmonie = new PlayerCard(3400, 150);
+        //Garulmonie
         PlayerCard Khenaga = new PlayerCard(3350, 200);
         PlayerCard Mulretonie = new PlayerCard(3000, 300);
         PlayerCard Nitralvie = new PlayerCard(3250, 250);
         PlayerCard Qaland = new PlayerCard(3500, 225);
-        PlayerCard Slovannoya = new PlayerCard(2850, 350);
+        //Slovannoya 
         PlayerCard Starvas = new PlayerCard(3550, 250);
         PlayerCard TheLeaf = new PlayerCard(3750, 150);
-        /*PlayerCard Traicere = new PlayerCard();
-        PlayerCard Yedesna = new PlayerCard();*/
+        PlayerCard Traicere = new PlayerCard(3600, 100);
+        PlayerCard Yedesna = new PlayerCard(3350, 250);*/
+        // Soldier cards (64 cards)
+        
+        //RESERVED FOR THE PROOF OF CONCEPT
+        SoldierCard Allemanie_A = new SoldierCard(150, 50);
+        SoldierCard Allemanie_B = new SoldierCard(100, 100);
+        SoldierCard Allemapon_A = new SoldierCard(150, 75);
+        SoldierCard Allemapon_B = new SoldierCard(100, 25);
+        SoldierCard Almahad_A = new SoldierCard(100, 70);
+        SoldierCard Almahad_B = new SoldierCard(100, 20);
+        SoldierCard Anglestan_A = new SoldierCard(200,35);
+        SoldierCard Anglestan_B = new SoldierCard(150,75);
+        SoldierCard Canalgeria_A = new SoldierCard(350,0);
+        SoldierCard Canalgeria_B = new SoldierCard(150,50);
+        SoldierCard Criota_A = new SoldierCard(150, 90);
+        SoldierCard Criota_B = new SoldierCard(200, 30);
+
+        /*SoldierCard Ekota_A = new SoldierCard(100,20);
+        SoldierCard Ekota_B = new SoldierCard(150, 50);
+        SoldierCard Finlonie_A = new SoldierCard(200,30);
+        SoldierCard Finlonie_B = new SoldierCard(200, 100);
+        SoldierCard Fitalie_A = new SoldierCard(150, 50);
+        SoldierCard Fitalie_B = new SoldierCard(150, 70);
+        SoldierCard Garulmonie_A = new SoldierCard(300, 10);
+        SoldierCard Garulmonie_B = new SoldierCard(150, 75);
+        SoldierCard Hongoru_A = new SoldierCard(150, 60);
+        SoldierCard Hongoru_B = new SoldierCard(200, 0);
+        SoldierCard Kaenia_A = new SoldierCard(100, 40);
+        SoldierCard Kaenia_B = new SoldierCard(180, 60);
+        SoldierCard Khenaga_A = new SoldierCard(150, 40);
+        SoldierCard Khenaga_B = new SoldierCard(150, 75);
+        SoldierCard Lirigant_A = new SoldierCard(175, 50);
+        SoldierCard Lirigant_B = new SoldierCard(150, 80);
+        SoldierCard Mageria_A = new SoldierCard(100, 50);
+        SoldierCard Mageria_B = new SoldierCard(150,50);
+        SoldierCard Maréquie_A = new SoldierCard(150, 50);
+        SoldierCard Maréquie_B = new SoldierCard(140, 40);*/
+       /* SoldierCard Mulretonie_A = new SoldierCard();
+        SoldierCard Mulretonie_B = new SoldierCard();
+        SoldierCard Neolantis_A = new SoldierCard();
+        SoldierCard Neolantis_B = new SoldierCard();
+        SoldierCard Nitralvie_A = new SoldierCard();
+        SoldierCard Nitralvie_B = new SoldierCard();
+        SoldierCard Onriance_A = new SoldierCard();
+        SoldierCard Onriance_B = new SoldierCard();
+        SoldierCard Qaland_A = new SoldierCard();
+        SoldierCard Qaland_B = new SoldierCard();
+        SoldierCard Slovanoya_A = new SoldierCard();
+        SoldierCard Slovanoya_B = new SoldierCard();
+        SoldierCard Starvas_A = new SoldierCard();
+        SoldierCard Starvas_B = new SoldierCard();
+        SoldierCard Suedemark_A = new SoldierCard();
+        SoldierCard Suedemark_B = new SoldierCard();
+        SoldierCard Suelly_A = new SoldierCard();
+        SoldierCard Suelly_B = new SoldierCard();
+        SoldierCard Tekay_A = new SoldierCard();
+        SoldierCard Tekay_B = new SoldierCard();
+        SoldierCard TheLeaf_A = new SoldierCard();
+        SoldierCard TheLeaf_B = new SoldierCard();
+        SoldierCard Traicere_A = new SoldierCard();
+        SoldierCard Traicere_B = new SoldierCard();
+        SoldierCard Tristan_A = new SoldierCard();
+        SoldierCard Tristan_B = new SoldierCard();
+        SoldierCard Tujar_A = new SoldierCard();
+        SoldierCard Tujar_B = new SoldierCard();
+        SoldierCard Ukraimea_A = new SoldierCard();
+        SoldierCard Ukraimea_B = new SoldierCard();
+        SoldierCard Yedesna_A = new SoldierCard();
+        SoldierCard Yedesna_B = new SoldierCard();*/
+        // Special cards (16 cards)
+        /* SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard
+         SpecialCard*/
+        // Object cards (14 cards)
+        /*ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard
+        ObjectCard*/
         #endregion
 
 
@@ -92,11 +199,30 @@ namespace LazniCardGame
             // }
 
         }
-        private void TeaTimeTactician(PlayerCard playerCard)
-        {
+        private void TeaTimeTactician(PlayerCard playerCard) { }
+        private void StrategicMentorship() { }
+        private void StrategicRetreat() { }
+        private void StandUpRoutine() { }
 
-        }
+
         #endregion
+
+        #region ACTIVE ABILITIES
+        private void StoicResolve() { }
+        private void DebatingDynamo() { }
+        private void AdaptivePunchline() { }
+        #endregion
+
+        private void SetCards()
+        {
+            PictureBox[] p1SecCards = { p1SecondaryCard1, p1SecondaryCard2, p1SecondaryCard3 };
+            PictureBox[] p2SecCards = { p2SecondaryCard1, p2SecondaryCard2, p2SecondaryCard3 };
+
+            /*for (int i = 0; i < p1SecCards.Length; i++) 
+            {
+                p1SecCards[i] = #RandomCard
+            }*/
+        }
 
         private void SetGame()
         {
