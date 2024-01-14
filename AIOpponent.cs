5dad1807c62fff0a8ef5f2c67a44db10a51e0057
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-01-14 15:49:38 -0500

    Finished the opponent paterns
    Also made the cards available externally for the releases if it's okay so people will have the cards in the releases

--- AIOpponent.cs
+++ AIOpponent.cs
@@ -5,8 +5,15 @@ using Microsoft.CodeAnalysis.Sarif.Driver;
 
 namespace LazniCardGame
 {
+
     public partial class Game : Form
     {
+        int numberOfP2CardsAlive;
+
+        IList<SecondaryCard> cardChosenByOpponent;
+
+        int[] indexesOpponentAttacks;
+
         // # of pattern the opponent will use when using its secondary card during the game
         public int noOpponentPlay;
 
@@ -17,25 +24,33 @@ namespace LazniCardGame
             Console.WriteLine("-------------------------------");
 #endif
             // Which cards each opponent cards will attack
-            IList<SecondaryCard> cardChosenByOpponent = p1SecCardsData.Shuffle();
+            cardChosenByOpponent = p1SecCardsData.Shuffle();
+
+            // Calculate the number of p2 cards
+            numberOfP2CardsAlive = HowManyp2CardLeft();
+
+            // List the indexes of the cards which can be attacked
+            indexesOpponentAttacks = CardAttackedIndexes();
 
             // Player card
             AttackCardCalculation(p2PlayerCardData, p1PlayerCardData);
 
-            AttackEachCardSeperately(cardChosenByOpponent);
-            /*  // Secondary cards (depends of which attack pattern the opponent uses)
-              switch (noOpponentPlay)
-              {
-                  case 0:
-                      AttackEachCardSeperately(cardChosenByOpponent);
-                      break;
-                  case 1:
-                      AttachEachCardAtATime(cardChosenByOpponent);
-                      break;
-                  case 2:
-                      AlternateBetweenEach(cardChosenByOpponent);
-                      break;
-              }*/
+            // Secondary cards (depends of which attack pattern the opponent uses)
+            if (p1SecondaryCard1.Visible && p1SecondaryCard2.Visible && p1SecondaryCard3.Visible)
+                switch (noOpponentPlay)
+                {
+                    case 0:
+                        AttackEachCardSeperately();
+                        break;
+                    case 1:
+                        AttachEachCardAtATime();
+                        break;
+                    case 2:
+                        AlternateBetweenEach();
+                        break;
+                }
+            else
+                AttackPlayerCard();
             UpdateCards();
         }
 
@@ -47,16 +62,20 @@ namespace LazniCardGame
             return count;
         }
 
-        private int[] CardAttackedIndexes(IList<SecondaryCard> p1Cards)
+        /// <summary>
+        /// Filter the indexes of the player's cards which have greater than 0hp.
+        /// Note that the original indexes are shuffled and then not directly linked to the position of the cards
+        /// </summary>
+        /// <returns>An array of integers for each card's index</returns>
+        private int[] CardAttackedIndexes()
         {
             int[] indexes = new int[3];
-            int numberOfCards = HowManyp2CardLeft();
-            for (int i = 0; i < numberOfCards; i++)
+            for (int i = 0; i < numberOfP2CardsAlive; i++)
             {
-                if (p1Cards[i].Hp <= 0)
+                if (cardChosenByOpponent[i].Hp <= 0)
                 {
                     for (int j = 0; j < 3; j++)
-                        if (p1Cards[j].Hp > 0)
+                        if (cardChosenByOpponent[j].Hp > 0)
                         {
                             indexes[i] = j;
                             break;
@@ -67,29 +86,44 @@ namespace LazniCardGame
             }
 #if DEBUG
             Console.Write("Opponent cards will attack:");
-            for (int i = 0; i < numberOfCards - 1; i++)
+            for (int i = 0; i < numberOfP2CardsAlive - 1; i++)
                 Console.Write($" {indexes[i]}");
-            Console.WriteLine($" {indexes[numberOfCards - 1]}");
+            Console.WriteLine($" {indexes[numberOfP2CardsAlive - 1]}");
             Console.WriteLine("-------------------------------");
 #endif
             return indexes;
         }
 
-        private void AttackEachCardSeperately(IList<SecondaryCard> p1Cards)
+        private void AttackEachCardSeperately()
         {
-            // Secondary Cards
-            int numberOfCards = HowManyp2CardLeft();
-            int[] indexes = CardAttackedIndexes(p1Cards);
-            for (int i = 0; i < p2SecCardsData.Length; i++)
-                AttackCardCalculation(p2SecCardsData[i], p1Cards[indexes[i]]);
+            int[] indexes = CardAttackedIndexes();
+            for (int i = 0; i < indexesOpponentAttacks.Length; i++)
+                AttackCardCalculation(p2SecCardsData[i], cardChosenByOpponent[indexesOpponentAttacks[i]]);
 
         }
 
-        private void AttachEachCardAtATime(IList<SecondaryCard> p1Cards)
+        private void AttachEachCardAtATime()
+        {
+            for (int i = 0; i < indexesOpponentAttacks.Length; i++)
+                AttackCardCalculation(p2SecCardsData[i], cardChosenByOpponent[indexesOpponentAttacks[0]]);
+        }
+
+        private void AlternateBetweenEach()
+        {
+            switch (random.Next(2))
+            {
+                case 0:
+                    AttackEachCardSeperately();
+                    break;
+                case 1:
+                    AttachEachCardAtATime();
+                    break;
+            }
+        }
+
+        private void AttackPlayerCard()
         {
 
         }
-
-        private void AlternateBetweenEach(IList<SecondaryCard> p1Cards) { }
     }
 }
--- Cheats.cs
+++ Cheats.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Windows.Forms;
-
-namespace LazniCardGame
+﻿namespace LazniCardGame
 {
-    /// <summary>
     /// THIS IS FOR DEBUG AND TESTING PURPOSES ONLY!
     /// Please set the cheats button to FALSE when doing a release (since visual studio somehow doesn't want to allow it to only appear when it's in 
-    /// </summary>
+
     public partial class Game
     {
         public void TheKiller()
         {
+#if DEBUG
             if (viewedCardPlayer == null)
             {
                 viewedCardSoldier.Hp = 0;
@@ -20,6 +17,12 @@ namespace LazniCardGame
                 viewedCardPlayer.Hp = 0;
             }
             UpdateCards();
+#else
+            // CHEATS SHOULD NEVER BE AVAILABLE IN A RELEASE
+            throw new System.Exception("Cheats are only available in DEBUG mode.");
+            // If this error happens, please set the property Visible of cheatsToolStripMenuItem1 in Game.cs [Design] or set cheatsToolStripMenuItem1.Visible to Game.Designer.cs to FALSE
+            // It is generated code, and for some reasons it cannot be changed while running the program, so it's safer to change it the first way
+#endif
         }
     }
 }
